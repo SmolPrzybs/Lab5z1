21b0c7fe45848d057472bbaf7fd1aacba29b3031
SmolPrzybs <maciek970@gmail.com>
2022-02-01 20:22:30 +0100

    Dodaj pliki projektów.

---
+++ Lab5z1/Lab5z1.cpp
@@ -0,0 +1,20 @@
+﻿# include <iostream>
+using namespace std;
+void bin(int);
+
+int main()
+{
+    cout << "Podaj liczbe dziesietnie: ";
+    int wartosc;
+    cin >> wartosc;
+    cout << "Liczba binarnie: ";
+    bin(wartosc);
+    cout << endl;
+    return 0;
+}
+void bin(int liczba)
+{
+    if (liczba >= 2)
+        bin(liczba / 2);
+    cout << liczba % 2;
+}
